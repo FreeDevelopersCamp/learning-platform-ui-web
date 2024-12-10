88051d0b7a65ec2f384641ed380a4dd50629c0d4
Bara Al-Sedih <98843912+BaraSedih11@users.noreply.github.com>
2024-12-10 14:34:50 +0200

    Update Sidebar.jsx

--- src/ui/Dashboard/Sidebar.jsx
+++ src/ui/Dashboard/Sidebar.jsx
@@ -2,12 +2,9 @@ import styled from 'styled-components';
 import MainNav from '../MainNav';
 
 const StyledSidebar = styled.aside`
-  position: fixed;
-  top: 6rem;
-  left: 0;
-  height: 100%;
-  width: ${(props) => (props.isOpen ? '12%' : '0')};
-  background-color: #000820;
+  width: ${(props) => (props.isOpen ? '12%' : '0px')};
+  /* background-color: #000820; */
+  background-color: var(--color-mutedblue-900);
   color: white;
   padding: ${(props) => (props.isOpen ? '8px 8px 0 0' : '0px')};
   transition: all 0.3s;
@@ -15,6 +12,7 @@ const StyledSidebar = styled.aside`
   display: flex;
   flex-direction: column;
   align-items: left;
+  /* gap: 1rem; */
 
   &:hover,
   &:active,
