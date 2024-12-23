9886d2b4e50692750e4b96c37946781d54e1aa7f
Yazan Al-Sedih <137224224+yazansedih@users.noreply.github.com>
2024-12-23 23:38:29 +0200

    Update CoursesLayout.jsx

--- src/ui/Layouts/CoursesLayout.jsx
+++ src/ui/Layouts/CoursesLayout.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { useAuth } from '../../contexts/auth/AuthContext';
-import { useGetUser } from '../../apis/core/User/hooks/useGetUser';
-import { useFetchProgressByUserId } from '../../hooks/learner/useProgress';
+import { useAuth } from '../contexts/auth/AuthContext';
+import { useSession } from '../hooks/auth/useSession';
+import { useUser } from '../hooks/users/useUser';
+import { useFetchProgressByUserId } from '../hooks/learner/useProgress';
+import { useFetchRoadmapById } from '../hooks/roadmaps/useRoadmap';
 
-import Header from '../Header/Header';
+import Header from './Dashboard/Header';
+import CoursesSidebar from '../ui/CoursesSidebar';
+import Spinner from './Spinner';
 
-import Spinner from '../Spinner';
-import CoursesSidebar from './../Sidebar/CoursesSidebar';
+import { FaListUl } from 'react-icons/fa';
 
 const StyledCoursesLayout = styled.div`
   display: flex;
@@ -21,58 +25,97 @@ const Main = styled.main`
   padding-top: var(--header-height);
   position: relative;
   display: flex;
+  align-items: flex-start;
+  justify-content: center;
   flex-grow: 1;
-  height: 100vh -var(--header-height);
+  height: calc(100vh - var(--header-height));
   background-color: var(--color-grey-0);
+  transition:
+    margin-left 0.3s ease-in-out,
+    width 0.3s ease-in-out;
 `;
 
 const Container = styled.div`
   display: flex;
-  align-items: flex-start;
-  width: 75%;
-  margin: 30px auto;
-  height: 100vh - var(--header-height);
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin: ${(props) =>
+    props.isSidebarOpen ? '3.5rem 0 3.5rem 14%' : '3.5rem auto'};
+  width: ${(props) => (props.isSidebarOpen ? '70%' : '90%')};
+  height: calc(100vh - var(--header-height));
+  padding: 3rem 1rem;
+  transition:
+    margin 0.3s ease-in-out,
+    width 0.3s ease-in-out;
+`;
+
+const Button = styled.div`
+  position: absolute;
+  top: 8rem;
+  left: 0;
+  border: 2px solid var(--color-grey-700);
+  border-radius: 5px;
+  border-top-right-radius: 50%;
+  border-bottom-right-radius: 50%;
+  padding: 1rem;
+  cursor: pointer;
+  transition: all 1s;
+
+  &:hover {
+    background-color: var(--color-grey-300);
+  }
 `;
 
 function CoursesLayout() {
-  const navigate = useNavigate();
+  const { roadmapId } = useParams();
+
   const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [courseStructure, setCourseStructure] = useState([]);
 
-  const { auth, session, isLoading } = useAuth();
-
-  const { user, isLoading: userLoading } = useGetUser(session?.username);
-
+  const { auth, isLoading } = useAuth();
+  const { isLoading: sessionLoading, session } = useSession();
+  const { user, isLoading: userLoading } = useUser(session?.username);
   const { data: userProgress, isLoading: userProgressLoading } =
     useFetchProgressByUserId(user?._id);
+  const { data: roadmap, isLoading: isLoadingRoadmap } =
+    useFetchRoadmapById(roadmapId);
 
   if (
     isLoading ||
+    sessionLoading ||
     userLoading ||
     userProgressLoading ||
+    isLoadingRoadmap ||
     !auth.isAuthenticated ||
     !user ||
-    !userProgress
+    !userProgress ||
+    !roadmap
   )
     return <Spinner />;
-
-  //   console.log('auth: ', auth);
-  //   console.log('session: ', session);
-  //   console.log('user: ', user);
-  //   console.log('userProgress: ', userProgress);
-
-  const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
 
   return (
     <StyledCoursesLayout>
       <Header page={'courses'} />
       <Main>
-        <CoursesSidebar toggleSidebar={toggleSidebar} />
-        <Container>
+        {!isSidebarOpen && (
+          <Button onClick={() => setSidebarOpen((prev) => !prev)}>
+            <FaListUl
+              style={{ fontSize: '2rem', color: 'var(--color-grey-700)' }}
+            />
+          </Button>
+        )}
+        <CoursesSidebar
+          isOpen={isSidebarOpen}
+          toggleSidebar={() => setSidebarOpen((prev) => !prev)}
+          roadmap={roadmap}
+        />
+        <Container isSidebarOpen={isSidebarOpen}>
           <Outlet
             context={{
-              session,
-              user,
-              userProgress,
+              roadmap,
+              courseStructure,
+              setCourseStructure,
             }}
           />
         </Container>
